1277d8d8fa372362c12eef3d4799bfb64c7f7c59
LuisCif-20 <n.rodas225@gmail.com>
2024-06-23 14:18:36 -0600

    auth-module inicializated

--- Proyecto2/frontend/src/app/auth/auth-routing.module.ts
+++ Proyecto2/frontend/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'sign-in',
+        title: 'Inicia Seción',
+        component: LoginPageComponent
+      },
+      {
+        path: 'sign-up',
+        title: 'Registrate',
+        component: RegisterPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'sign-in',
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
