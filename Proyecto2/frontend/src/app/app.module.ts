77b3b3e0b6200560b16d19767ee8e25ede7fbc69
David15Barrera <david1315barrera@gmail.com>
2024-06-24 01:35:35 -0600

    Start-User Proposal

--- Proyecto2/frontend/src/app/app.module.ts
+++ Proyecto2/frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './user/inicio/inicio.component';
+import { CatalogoComponent } from './user/catalogo/catalogo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule,
