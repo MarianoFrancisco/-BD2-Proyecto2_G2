d625b6cf5a0d95304f5fb8dfa9815bfd207be544
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2024-06-24 21:11:07 -0600

    update: aws service implemented for images

--- Proyecto2/backend/app/routes/author.routes.js
+++ Proyecto2/backend/app/routes/author.routes.js
@@ -8,10 +8,11 @@ const router = express.Router();
 import protect from '../middleware/protectMiddleware.js';
 import { getAuthors, addAuthor, deleteAuthor } from '../controllers/authorController.js';
 import { admin } from '../middleware/roleMiddleware.js';
+import { upload } from '../middleware/uploadMiddleware.js';
 
 router.get('', protect, getAuthors);
 
-router.post('', protect, admin, addAuthor);
+router.post('', protect, admin, upload.single('imagen'), addAuthor);
 
 router.delete('/:id', protect, admin, deleteAuthor);
 
