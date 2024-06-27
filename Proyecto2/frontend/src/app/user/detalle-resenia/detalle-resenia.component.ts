8be7ca0cb5a55724b47bb0592ab1104673f87b86
Angel Aragón <69768799+Angelgt3@users.noreply.github.com>
2024-06-27 10:03:52 -0600

    correcion de bug reseña

--- Proyecto2/frontend/src/app/user/detalle-resenia/detalle-resenia.component.ts
+++ Proyecto2/frontend/src/app/user/detalle-resenia/detalle-resenia.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LibroService } from '../../shared/services/libro.service';
 import { Book, Review } from '../../models';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-detalle-resenia',
@@ -14,11 +15,13 @@ export class DetalleReseniaComponent implements OnInit {
   calificacion: number = 0;
   resena: string = '';
   resenias: Review[] = [];
+  calificacionPromedio: number = 0;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private libroService: LibroService
+    private libroService: LibroService,
+    private toastr:ToastrService
   ) { }
 
   ngOnInit(): void {
@@ -33,10 +36,11 @@ export class DetalleReseniaComponent implements OnInit {
     this.libroService.getLibroById(id).subscribe(
       (libro: Book) => {
         this.libro = libro;
-        console.log(this.libro)
+        this.actualizarCalificacionPromedio();
       },
       error => {
         console.error('Error al obtener el detalle del libro', error);
+        this.toastr.error('Error al obtener el detalle del libro', 'Server');
       }
     );
   }
@@ -45,21 +49,37 @@ export class DetalleReseniaComponent implements OnInit {
     this.libroService.getReseñasByLibro(libroId).subscribe(
       (resenias: Review[]) => {
         this.resenias = resenias;
+        this.actualizarCalificacionPromedio();
         console.log(this.resenias)
       },
       error => {
         console.error('Error al obtener las reseñas del libro', error);
+        this.toastr.error('Error al obtener las reseñas del libro', 'Server');
       }
     );
   }
 
-  escribirResena(): void {
-    console.log('Calificación:', this.calificacion);
-    console.log('Reseña:', this.resena);
+  actualizarCalificacionPromedio(): void {
+    if (this.resenias.length > 0) {
+      const totalPuntuaciones = this.resenias.reduce((acc, resenia) => acc + resenia.puntuacion, 0);
+      this.calificacionPromedio = totalPuntuaciones / this.resenias.length;
+    } else {
+      this.calificacionPromedio = 0;
+    }
+  }
 
+  hoverEstrella(calificacion: number): void {
+    this.calificacion = calificacion;
+  }
+
+  seleccionarCalificacion(calificacion: number): void {
+    this.calificacion = calificacion;
+  }
+
+  escribirResena(): void {
     this.libroService.guardarReseña(this.libroId, this.resena, this.calificacion).subscribe(
       (response) => {
-        console.log('Reseña guardada exitosamente', response);
+        this.toastr.success('Reseña guardada exitosamente', 'Server');
         // Actualizar la lista de reseñas después de guardar la nueva reseña
         this.getReseñasByLibro(this.libroId);
         // Limpiar los campos de calificación y reseña después de guardar
@@ -67,18 +87,27 @@ export class DetalleReseniaComponent implements OnInit {
         this.resena = '';
       },
       (error) => {
-        console.error('Error al guardar la reseña', error);
-        if (error.error === 'You have already reviewed this book') {
-          alert('Ya has realizado una reseña de este libro anteriormente.');
+        //console.error('Error al guardar la reseña', error);
+        if (error.error.error === 'You have already reviewed this book') {
+          this.toastr.error('Ya has realizado una reseña de este libro anteriormente.','server');
         } else {
-          alert('Ocurrió un error al guardar la reseña. Por favor, intenta nuevamente más tarde.');
+          this.toastr.error('Ocurrió un error al guardar la reseña. Por favor, intenta nuevamente más tarde.', 'Server');
         }
       }
     );
   }
 
   generarEstrellas(calificacion: number): number[] {
-    return Array(calificacion).fill(0).map((x, i) => i + 1);
+    const entero = Math.floor(calificacion); 
+    const decimal = calificacion - entero;   
+    let estrellas: number[] = [];
+    for (let i = 0; i < entero; i++) {
+      estrellas.push(i + 1);
+    }
+    if (decimal >= 0.25 && decimal <= 0.75) {
+      estrellas.push(0.5); 
+    }
+    return estrellas;
   }
 
   volver(): void {
