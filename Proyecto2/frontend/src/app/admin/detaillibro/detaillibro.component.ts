d91be0f858d2ea1b48cf9b068f4073056ecbd80f
Angel Aragón <69768799+Angelgt3@users.noreply.github.com>
2024-06-26 23:42:28 -0600

    registro libro

--- Proyecto2/frontend/src/app/admin/detaillibro/detaillibro.component.ts
+++ Proyecto2/frontend/src/app/admin/detaillibro/detaillibro.component.ts
@@ -1,7 +1,9 @@
 import { Libro } from './../../user/interfaces/order.interface';
 import { LibroService } from '../../user/services/libro.service';
+import { RegistryBooksService } from '../service/registry-books.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router  } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-detaillibro',
@@ -10,13 +12,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetaillibroComponent implements OnInit {
   libro: Libro | undefined | any;
+  id: string ='';
 
-  constructor(private route: ActivatedRoute, private libroService: LibroService) { }
+  constructor(private route: ActivatedRoute, private libroService: LibroService, private registryBooksService: RegistryBooksService, private router:Router,private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const id = params['id'];
       if (id) {
+        this.id=id;
         this.libroService.getLibroById(id).subscribe(libro => {
           this.libro = libro;
         });
@@ -37,4 +41,29 @@ export class DetaillibroComponent implements OnInit {
     console.log(`Calificado con ${star} estrellas`);
     alert(`Calificado con ${star} estrellas`);
   }
+
+  confirmDelete(): void {
+    this.toastr.warning('Presioneme si está seguro de que deseas eliminar este libro', 'Confirmar', {
+      positionClass: 'toast-top-center',
+      closeButton: true,
+      disableTimeOut: true,
+    }).onTap.subscribe(() => {
+      this.deleteLibro();
+    });
+  }
+
+  deleteLibro(): void {
+    this.registryBooksService.deleteBookById(this.id).subscribe(
+      response => {
+       //console.log('Libro eliminado', response);
+        this.toastr.success('Libro eliminado', 'Server');
+        this.router.navigate(['/admin/view-book']);
+      },
+      error => {
+        //console.error('Error al eliminar el libro', error);
+        this.toastr.error('Error al eliminar el libro', 'Server');
+      }
+    );
+  }
+
 }
