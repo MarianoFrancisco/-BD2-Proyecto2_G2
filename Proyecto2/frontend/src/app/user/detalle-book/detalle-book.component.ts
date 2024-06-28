b5f79e3e774c0b9ac17650920370e1ce8d395490
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2024-06-27 19:49:48 -0600

    [user] agregar al carrito desde detalles del libro, con input de cantidad.

--- Proyecto2/frontend/src/app/user/detalle-book/detalle-book.component.ts
+++ Proyecto2/frontend/src/app/user/detalle-book/detalle-book.component.ts
@@ -1,9 +1,13 @@
 import { Libro } from './../interfaces/books.interface';
 import { LibroService } from '../services/libro.service';
 import { RegistryBooksService } from '../../admin/service/registry-books.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Review } from '../../models';
+import Swal from 'sweetalert2';
+import { LibroPedido } from '../../shopping/interfaces/libro-pedido';
+import { ShoppingServieService } from '../../shopping/services/shopping-servie.service';
+
 
 @Component({
   selector: 'app-detalle-book',
@@ -17,6 +21,8 @@ export class DetalleBookComponent implements OnInit {
   defaultImageUrl: string = '../../../assets/libro.png';
   quantity: number = 1;
   isValidQuantity: boolean = true;
+
+  private readonly shoppingService = inject(ShoppingServieService)
 
   constructor(private route: ActivatedRoute, private libroService: LibroService,private registryBooksService: RegistryBooksService) { }
 
@@ -53,8 +59,50 @@ export class DetalleBookComponent implements OnInit {
     event.target.src = this.defaultImageUrl;
   }
 
-  addToCart(){
-
+  addToCart(): void {
+    console.log(this.libro);
+    
+    if (!this.libro.disponibilidad || this.libro.cantidad_stock < this.quantity) {
+      Swal.fire({
+        title: "No Disponible",
+        text: "El libro no se encuentra disponible, intente mas tarde",
+        icon: "info"
+      });
+      return
+    }
+    const libroShopp: LibroPedido = {
+      titulo: this.libro.titulo,
+      autor_id: this.libro.autor_id._id,
+      disponibilidad: this.libro.disponibilidad,
+      cantidad_stock: this.libro.cantidad_stock,
+      precio: this.libro.precio,
+      cantidad_pedido: 1,
+      libro_id : this.libro._id
+    }
+    
+    const exist = this.shoppingService.librosPedio.find(li => li.libro_id === libroShopp.libro_id)
+    if (exist) {
+      if (exist.cantidad_stock < exist.cantidad_pedido+this.quantity) {
+        Swal.fire({
+          title: "No Disponible",
+          text: "El libro no se encuentra disponible, intente mas tarde",
+          icon: "info"
+        });
+        return
+      }
+      exist.cantidad_pedido += this.quantity;
+    }else{
+      console.log(libroShopp);
+      
+      this.shoppingService.librosPedio.push(libroShopp)
+    }
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "El libro se agrego al carrito",
+      showConfirmButton: false,
+      timer: 1200
+    });
   }
 
   validateQuantity(): void {
